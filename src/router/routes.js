280c94e4889398a26f8327dc0dfd66a4393cf481
Your Name <you@example.com>
2022-12-09 10:37:00 +0800

    pdate

---
+++ src/router/routes.js
@@ -0,0 +1,14 @@
+import Home from '@/pages/Home'
+import Login from '@/pages/Login'
+// import Register from '@/pages/Register'
+// import Search from '@/pages/Search'
+export default [
+    {
+        path:'/',
+        component:Home,
+    },
+    {
+        path:'/login',
+        component:Login
+    }
+] 
